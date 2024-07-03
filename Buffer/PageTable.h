d68897959b742c5fd9bab7f1276b5ea9afdc7b19
2023JoshuaP <jphilco@unsa.edu.pe>
2024-07-03 18:18:51 -0500

    Se cambio unas funciones que no tenian nada que ver.

--- Buffer/PageTable.h
+++ Buffer/PageTable.h
@@ -10,7 +10,7 @@ using namespace std;
 class PageTable {
     public:
         int sizeColumnaFrameId;
-        int columnasPageTable = 6;
+        int columnasPageTable = 5;
         vector<vector<int>> matrizPageTable;
     public:
         PageTable();
@@ -22,26 +22,19 @@ class PageTable {
         void aumentarPinCountDePagina(int numPagina);
         void pinnedAPagina(int numPagina);
         void unpinnedAPagina(int numPagina);
-        void cambiarDirtyBitDePaginaA1(int numPagina);
-        void cambiarDirtyBitDePaginaA0(int numPagina);
+        void cambiarDirtyBitDePagina(int numPagina);
         void renovarLastUsedDePagina(int numPagina);
         void aumentarLastUsedDePagina(int numPagina);
         void actualizarPageTableSolicitandoPaginaNueva(int numPaginaActualizar, int numFilaFrameId);
         void aumentarLastUsedDeTodasLasPaginas(int numFrameIgnorar);
-        bool verificarSiPinCountDePaginasEnCero();
-        bool verificarSiPaginaTienePinCountEnCero(int numPagina);
-        bool verificarSiExistePaginasUnpinned();
-        bool verificarSiPaginaEstaPinned(int numPagina);
         bool verificarSiHayFrameParaIgnorar(vector<int>& vectorFramesIgnorar, int FrameIdParaEncontrar);
-        void aplicarAlgoritmoLRU(int numPagina, vector<int> &vectorFramesAignorar, int &numPaginaReemplazar, bool &eliminarPageSinEscrituraEnDisco, bool &eliminarPageConEscrituraEnDisco);
+        //void aplicarAlgoritmoLRU(int numPagina, vector<int> &vectorFramesAignorar, int &numPaginaReemplazar, bool &eliminarPageSinEscrituraEnDisco, bool &eliminarPageConEscrituraEnDisco);
         string analizarPageTableParaAgregarPagina(int numPagina);
         bool verificarFramesLlenos();
         void actualizarDetallesDePaginaEliminada(int numPaginaActualizar, int nuevaPaginaActualizar);
         void activarReferentBit(int numPagina);
-        void desactivarReferentBit(int numPagina);
-        void aplicarAlgoritmoClock(int numPagina, vector<int> &vectorFramesConPinCount0, int &numPaginaReemplazar, bool &eliminarPageSinEscrituraEnDisco, bool &eliminarPageConEscrituraEnDisco);
-        void recolectarFramesPinCountEnCeroYUnpinned(vector<int>& vectorFramesPinCountEn0);
-        bool verificarSiDirtyBitEstaSucio(int numPagina);
+        void aplicarAlgoritmoClock(int numPagina, int numFrameAignorar, bool &eliminarPageSinEscrituraEnDisco, bool &eliminarPageConEscrituraEnDisco, int &numPaginaReemplazar);
+        void aplicarAlgoritmoClockConCondicionPinned(int numPagina, int numFrameAignorar, bool &eliminarPageSinEscrituraEnDisco, bool &eliminarPageConEscrituraEnDisco, int &numPaginaEliminada);
 };
 
 #endif
